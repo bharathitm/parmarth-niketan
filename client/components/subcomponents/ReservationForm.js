32d6defb45326201e88b89808bd66bf535edbc9e
bharathitm <39156453+bharathitm@users.noreply.github.com>
2020-01-12 22:11:49 +0530

    Changes to reservatino check in form

--- client/components/subcomponents/ReservationForm.js
+++ client/components/subcomponents/ReservationForm.js
@@ -10,10 +10,10 @@ export class ReservationForm extends Component {
 
     constructor(props) {
       super(props);
-  
+
       this.state = {
         ReservationDetails:  [{}]
-      }; 
+      };
     }
 
     componentDidMount(){
@@ -55,10 +55,10 @@ export class ReservationForm extends Component {
             }
         } else {
             notify.show('Oops! Something went wrong! Please try again!', 'error');
-        } 
+        }
     }
 
-     
+
     render() {
         return(
                 <div id="divReservationFormContents" style={{width:'100%', visibility:'hidden', display:'none'}}>
@@ -66,10 +66,10 @@ export class ReservationForm extends Component {
                               <h4 style={{margin: 0, textAlign: 'center'}}>SWAMI SHUKDEVANAND TRUST</h4>
                               <h4 style={{margin: 0, textAlign: 'center'}}>Parmarth Niketan</h4>
                               <h4 style={{margin: 0, textAlign: 'center'}}>Rishikesh 249304</h4><br/>
-                              <h5 style={{margin: 0, textAlign: 'center'}}>(In The Welfare of All)</h5><br/><br/>
-                              <h3 style={{margin: 0, textAlign: 'center'}}><u>CHECK IN FORM No. {this.state.ReservationDetails[0].reservation_id}</u></h3>              
-                       
-                              <div id="divReservationForm" style={{marginTop:'1em', padding:'0.5em'}}>  
+                              {/* <h5 style={{margin: 0, textAlign: 'center'}}>(In The Welfare of All)</h5><br/><br/> */}
+                              <h3 style={{margin: 0, textAlign: 'center'}}><u>CHECK IN FORM No. {this.state.ReservationDetails[0].reservation_id}</u></h3>
+
+                              <div id="divReservationForm" style={{marginTop:'1em', padding:'0.5em'}}>
 
                               <table cellPadding="7" style={{width: '99.5%', textAlign:'left', borderCollapse: 'collapse', border: '1px solid black'}}>
                                     <tbody>
@@ -108,47 +108,72 @@ export class ReservationForm extends Component {
                                                 <td style={{width:'50%'}}># of Guests:</td>
                                                   <td>{this.state.ReservationDetails[0].no_of_people}</td>
                                                 </tr>
+                                                <tr>
+                                                <td style={{width:'50%'}}>Purpose:</td>
+                                                  <td>
+                                                    {/* {this.state.ReservationDetails[0].no_of_people} */}
+                                                    </td>
+                                                </tr>
                                                 </tbody>
                                               </table>
                                           </td>
                                             <td style={{width:'25%', verticalAlign:'top'}}>
                                             {this.state.ReservationDetails[0].e_first_name} {this.state.ReservationDetails[0].e_last_name} <br/>
                                             {this.state.ReservationDetails[0].e_phone_no} <br/>
-                                            {this.state.ReservationDetails[0].e_relationship}
+                                            {this.state.ReservationDetails[0].e_relationship} <br/>
+                                            <b>ID No:</b><br/>
+                                            <b>Vehicle No:</b>
                                           </td>
                                         </tr>
                                         </tbody>
                                         </table>
                               <br/>
-                              <div style={{marginBottom:'25em'}}>
-                                    <h3>Guest(s)</h3>
+                              <div style={{marginBottom:'24em'}}>
+                                <table style={{width: '100%'}}>
+                                  <tr>
+                                    <td>
+                                      <span style={{fontWeight: 'bolder'}}>Guest(s)</span>
+                                    </td>
+                                    <td style={{width:'75%', paddingRight:'1.5em', textAlign: 'right'}}>
+                                    Male: ____ Female: ____ Children: _____
+                                    </td>
+                                  </tr>
+                                  </table>
+                                  
+                                    <br/>
                                     <table cellPadding="10" style={{width: '99%', textAlign:'left', borderCollapse: 'collapse', border: '1px solid black'}}>
                                             <tbody>
                                                 <tr style={{border: '1px solid black'}}>
                                                   <th>Name</th>
-                                                  <th>Phone</th>
-                                                  <th>Email</th>
+                                                  <th>Age</th>
                                                   <th>Relation</th>
+                                                  <th>Phone/Email</th>                          
+                                                  <th>Medical Condition</th>                                                  
                                                 </tr>
                                                 <tr>
                                                   <td>{this.state.ReservationDetails[0].first_name} {this.state.ReservationDetails[0].last_name}</td>
-                                                  <td>{this.state.ReservationDetails[0].phone_no}</td>
-                                                  <td>{this.state.ReservationDetails[0].email_id}</td>
+                                                  <td></td>
                                                   <td>PRIMARY</td>
+                                                  <td>{this.state.ReservationDetails[0].phone_no} / 
+                                                  {this.state.ReservationDetails[0].email_id}</td>
+                                                  <td></td>                                                  
                                                 </tr>
                                               </tbody>
                                           </table>
                               </div>
 <b>I hereby declare the above information as accurate and assure you to abide by the regulations of the ashram.</b>
 <br/><br/>
-Signature: 
+Signature:
 <br/><br/>
-Checked In By: 
+<hr/>
+<i>For Office Use Only</i><br/>
+Room No: __________ &nbsp; Block: __________ &nbsp; Receipt No: __________<br/>
+Checked In By:
           </div>
           </div>
                </div>
                );
         }
       }
-  
+
     export default ReservationForm;
